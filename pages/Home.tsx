6eeae3f12be9cc12581a4df46d4a30cb20e92cdb
Andhika Ventausa <andhika.ventausa@gmail.com>
2020-09-23 20:15:59 +0700

    text on borderColor causing break

--- pages/Home.tsx
+++ pages/Home.tsx
@@ -1,6 +1,6 @@
 /*CORE*/
 import React from 'react';
-import {Button, StyleSheet, View} from 'react-native';
+import {Button, StyleSheet, View, Text} from 'react-native';
 /*LIBS*/
 import appboxosdk from '@appboxo/react-native-sdk';
 
@@ -9,12 +9,19 @@ interface Props {
 }
 
 export default function Home({navigation}: Props) {
+  const [styling, setStyling] = React.useState({});
   React.useEffect(() => {
     appboxosdk.setConfig('[client_id]', false); //set your Appboxo client id
   }, []);
 
   return (
     <View style={styles.container}>
+      <Text style={styling} >
+        Click `Hit Me!!` button to make it crash when applying borderColor to Text :D
+      </Text>
+      <Button 
+        title="Hit Me!!"
+        onPress={() => {setStyling({borderColor: 'red'})}} />
       <Button
         title="Go to first screen"
         onPress={() => navigation.navigate('FirstScreen')}
